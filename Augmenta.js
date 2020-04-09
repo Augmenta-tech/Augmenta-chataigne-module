dcb92d389daca86cc02d665a265cc7f3b430b7ff
David-Alexandre Chanel <david.alexandre.chanel.ext@gmail.com>
2020-04-09 13:20:55 +0200

    added parsing and display of /extra values

--- Augmenta.js
+++ Augmenta.js
@@ -28,7 +28,7 @@ function moduleParameterChanged(param)
 {
 	if(param.is(local.parameters.singleObjectMode)) {
 
-		if(local.parameters.singleObjectMode.getData() == "none")
+		if(local.parameters.singleObjectMode.get() == "none")
 		{
 			// Clean and fold single person object
 			local.values.singleObject.setCollapsed(true);
@@ -37,6 +37,23 @@ function moduleParameterChanged(param)
 		} else {
 			// Unfold single person object panel
 			local.values.singleObject.setCollapsed(false);
+		}
+	} else if(param.is(local.parameters.displayObjectExtraData))
+	{
+		if(local.parameters.displayObjectExtraData.get())
+		{
+			local.values.singleObject.getChild("Extra").setCollapsed(false);
+			for (var i = 0; i < maxObjectsDisplayed; i++) {
+
+ 				local.values.getChild("object"+i).getChild("Extra").setCollapsed(false);
+			}
+		} else
+		{
+			local.values.singleObject.getChild("Extra").setCollapsed(true);
+			for (var i = 0; i < maxObjectsDisplayed; i++) {
+
+ 				local.values.getChild("object"+i).getChild("Extra").setCollapsed(true);
+			}
 		}
 	}
 }
@@ -116,8 +133,77 @@ function oscEvent(address,args)
 
 		} else if(local.parameters.singleObjectMode.getData() == "newest" && args[2] == getNewestId())
 		{
-			
 			resetAugmentaObject(local.values.singleObject);
+		}
+	} else if(address == "/object/update/extra")
+	{
+
+		// Update objects
+		for(var i = 0 ; i < maxObjectsDisplayed ; i++)
+		{
+			 if(args[2] == i) // //args[2] = oid
+			 {
+ 				setAugmentaExtraObject(local.values.getChild("object" + i), args);
+			 }
+		}
+
+		// Update Oldest and newest
+		// Oldest is always oid = 0 if algo is correctly implemented
+		if(local.parameters.singleObjectMode.getData() == "oldest" && args[2] == 0)
+		{
+			setAugmentaExtraObject(local.values.singleObject, args);
+
+		} else if(local.parameters.singleObjectMode.getData() == "newest" && args[2] == getNewestId())
+		{
+
+			setAugmentaExtraObject(local.values.singleObject, args);
+
+		}
+
+	} else if(address == "/object/enter/extra")
+	{
+		// Update objects
+		for(var i = 0 ; i < maxObjectsDisplayed ; i++)
+		{
+			 if(args[2] == i) // //args[2] = oid
+			 {
+ 				local.values.getChild("object" + i).setCollapsed(false);
+ 				setAugmentaExtraObject(local.values.getChild("object" + i), args);
+			 }
+		}
+
+		// Update Oldest and newest
+		// Oldest is always oid = 0 if algo is correctly implemented
+		if(local.parameters.singleObjectMode.getData() == "oldest" && args[2] == 0)
+		{
+			setAugmentaExtraObject(local.values.singleObject, args);
+
+		} else if(local.parameters.singleObjectMode.getData() == "newest" && args[2] == getNewestId())
+		{
+			setAugmentaExtraObject(local.values.singleObject, args);
+		}
+
+	} else if(address == "/object/leave")
+	{
+		
+		for(var i = 0 ; i < maxObjectsDisplayed ; i++)
+		{
+			 if(args[2] == i) // //args[2] = oid
+			 {
+ 				local.values.getChild("object" + i).setCollapsed(true);
+ 				resetAugmentaExtraObject(local.values.getChild("object" + i), args);
+			 }
+		}
+
+		// Reset Oldest and newest
+		// Oldest is always oid = 0 if algo is correctly implemented
+		if(local.parameters.singleObjectMode.getData() == "oldest" && args[2] == 0)
+		{
+			resetAugmentaExtraObject(local.values.singleObject);
+
+		} else if(local.parameters.singleObjectMode.getData() == "newest" && args[2] == getNewestId())
+		{
+			resetAugmentaExtraObject(local.values.singleObject);
 		}
 	}
 }
@@ -139,6 +225,16 @@ function setAugmentaObject(object, args)
 	object.height.set(args[14]);
 }
 
+function setAugmentaExtraObject(object, args)
+{
+	object.frame.set(args[0]);
+	object.id.set(args[1]);
+	object.oid.set(args[2]);
+	object.highest.set(args[3],args[4]);
+	object.distance.set(args[5]);
+	object.reflectivity.set(args[6]);
+}
+
 function resetAugmentaObject(object)
 {
 	object.hasData.set(false);
@@ -154,6 +250,16 @@ function resetAugmentaObject(object)
 	object.boundingRectHeight.set(0);
 	object.boundingRectRotation.set(0);
 	object.height.set(0);
+}
+
+function resetAugmentaExtraObject(object)
+{
+	object.frame.set(0);
+	object.id.set(0);
+	object.oid.set(0);
+	object.highest.set(0,0);
+	object.distance.set(0);
+	object.reflectivity.set(0);
 }
 
 function setAugmentaScene(scene, args)
